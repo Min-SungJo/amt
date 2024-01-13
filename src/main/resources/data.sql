8debedd30b6324afaac8296ae37df5cc3fe029d3
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2024-01-13 14:44:39 +0900

    sql for test

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10001, 'ride', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10002, 'ride', 'Learn Linux', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10003, 'ride', 'Learn Spring', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10004, 'ride', 'Lean AWS', CURRENT_DATE(), false);
